ccfc47c927f663477f6cda693c8d24a6e28634e2
diego tonini <diego.tonini93@gmail.com>
2024-05-30 21:52:56 +0200

    feat: add new page

--- src/screen/allScreen/page.tsx
+++ src/screen/allScreen/page.tsx
@@ -48,6 +48,75 @@ export const AllScrren: Component<{ screen: MyStore["screen"] }> = (props) => {
           </div>
         </div>
       </Show>
+
+      <Show when={props.screen.screenName === "scelta-piada"}>
+        <div>
+          <div
+            style={{
+              "font-size": "2rem",
+            }}
+          >
+            <p>PIADA PICCOLA: 1 GUSTO</p>
+            <p>PIADA MEDIA: 2 GUSTI</p>
+            <p>PIADA GRANDE: 3 GUSTI</p>
+          </div>
+
+          <div
+            style={{
+              position: "relative",
+              width: "90vw",
+              height: "60vh",
+            }}
+          >
+            <div
+              style={{
+                position: "absolute",
+                left: "50%",
+                transform: "translateX(-50%)",
+              }}
+            >
+              <Banner
+                color="orange"
+                label="AFFETTATI"
+                type="text"
+                width="30vw"
+              />
+            </div>
+            <div
+              style={{
+                position: "absolute",
+                right: "5%",
+                top: "50%",
+                transform: "translateY(-50%)",
+              }}
+            >
+              <Banner color="green" label="VERDURE" type="text" width="30vw" />
+            </div>
+
+            <div
+              style={{
+                position: "absolute",
+                left: "50%",
+                transform: "translateX(-50%)",
+                bottom: 0,
+              }}
+            >
+              <Banner color="purple" label="DOLCi" type="text" width="30vw" />
+            </div>
+
+            <div
+              style={{
+                position: "absolute",
+                left: "5%",
+                top: "50%",
+                transform: "translateY(-50%)",
+              }}
+            >
+              <Banner color="blue" label="FORMAGGI" type="text" width="30vw" />
+            </div>
+          </div>
+        </div>
+      </Show>
     </div>
   );
 };
