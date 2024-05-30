2cd0422d28af807a425b751c798aba7c4f9e37f1
diego tonini <diego.tonini93@gmail.com>
2024-05-30 19:11:44 +0200

    feat: add navigation from Banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,7 +1,9 @@
 import { Show, type Component } from "solid-js";
 import styles from "./banner.module.css";
+import { Screen } from "../../types";
+import { useMyStore } from "../../StoreProvider";
 
-type Props = { width: string } & (
+type Props = { width: string; goToScreen?: Screen["screenName"] } & (
   | {
       type: "img";
       url: string;
@@ -14,12 +16,15 @@ type Props = { width: string } & (
 );
 
 export const Banner: Component<Props> = (props) => {
+  const { setScreen } = useMyStore();
+
   return (
     <div
       class={styles.container}
       style={{
         width: props.width,
       }}
+      onClick={props.goToScreen ? [setScreen, props.goToScreen] : undefined}
     >
       {props.type === "img" && <img class={styles.image} src={props.url} />}
       {props.type === "text" && (
--- src/screen/allScreen/page.tsx
+++ src/screen/allScreen/page.tsx
@@ -1,14 +1,14 @@
-import { Index, Show, type Component } from "solid-js";
+import { Show, type Component } from "solid-js";
 import styles from "./screen.module.css";
 import { MyStore } from "../../types";
 import { Banner } from "../../components/Banner";
 import { HOME_SCREEN_ASSETS } from "./assets";
 
-export const AllScrren: Component<{ screen: MyStore["screen"] }> = (props) => {
-  const banner1 = HOME_SCREEN_ASSETS[0];
-  const banner2 = HOME_SCREEN_ASSETS[1];
-  const banner3 = HOME_SCREEN_ASSETS[2];
+const banner1 = HOME_SCREEN_ASSETS[0];
+const banner2 = HOME_SCREEN_ASSETS[1];
+const banner3 = HOME_SCREEN_ASSETS[2];
 
+export const AllScrren: Component<{ screen: MyStore["screen"] }> = (props) => {
   return (
     <div class={styles.container}>
       <Show when={props.screen.screenName === "home"}>
@@ -19,7 +19,12 @@ export const AllScrren: Component<{ screen: MyStore["screen"] }> = (props) => {
               "justify-content": "center",
             }}
           >
-            <Banner type="img" url={banner1.url} width="40vw" />
+            <Banner
+              type="img"
+              url={banner1.url}
+              width="40vw"
+              goToScreen={banner1.goToScreen}
+            />
           </div>
           <div
             style={{
@@ -28,8 +33,18 @@ export const AllScrren: Component<{ screen: MyStore["screen"] }> = (props) => {
               gap: "3rem",
             }}
           >
-            <Banner type="img" url={banner2.url} width="40vw" />
-            <Banner type="img" url={banner3.url} width="40vw" />
+            <Banner
+              type="img"
+              url={banner2.url}
+              width="40vw"
+              goToScreen={banner2.goToScreen}
+            />
+            <Banner
+              type="img"
+              url={banner3.url}
+              width="40vw"
+              goToScreen={banner3.goToScreen}
+            />
           </div>
         </div>
       </Show>
